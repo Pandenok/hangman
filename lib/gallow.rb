25a9c95d04d319d7ecb8e4fc7bcbd1f20b97aaae
pandenok <dimitri.shatunin@gmail.com>
2020-07-04 18:14:18 +0200

    Draw gallow in base of guesses

--- lib/gallow.rb
+++ lib/gallow.rb
@@ -1,2 +1,97 @@
 module Gallow
+  def initial_setup(secret_word, guess)
+    <<~HEREDOC
+    +---+     
+    |        WORD:  #{secret_word}
+    |          
+    |        GUESS: #{guess}
+    |
+    |
+    =========
+    HEREDOC
+  end
+  
+  def draw_head(secret_word)
+    <<~HEREDOC
+    +---+     
+    |         WORD:   #{secret_word}  
+    |   0       
+    |         GUESS:  
+    |         
+    |         MISSES:  
+    =========
+    HEREDOC
+  end
+  
+  def draw_body(secret_word)
+    <<~HEREDOC
+    +---+     
+    |         WORD:  #{secret_word}
+    |   0       
+    |   |     GUESS:
+    |         
+    |         MISSES:
+    =========
+    HEREDOC
+  end
+  
+  def draw_right_arm(secret_word)
+    <<~HEREDOC
+    +---+     
+    |         WORD:  #{secret_word}  
+    |   0        
+    |   |\    GUESS:  
+    |         
+    |         MISSES:
+    =========
+    HEREDOC
+  end
+  
+  def draw_left_arm(secret_word)
+    <<~HEREDOC
+    +---+     
+    |         WORD:  #{secret_word}
+    |   0       
+    |  /|\    GUESS:
+    |         
+    |         MISSES:
+    =========
+    HEREDOC
+  end
+  
+  def draw_right_leg(secret_word)
+    <<~HEREDOC
+    +---+     
+    |         WORD:  #{secret_word}
+    |   0       
+    |  /|\    GUESS:
+    |    \     
+    |         MISSES:
+    =========
+    HEREDOC
+  end
+  
+  def draw_left_leg(secret_word)
+    <<~HEREDOC
+    +---+     
+    |         WORD:  #{secret_word}
+    |   0       
+    |  /|\    GUESS:
+    |  / \     
+    |         MISSES:
+    =========
+    HEREDOC
+  end
+  
+  def draw_noose(secret_word)
+    <<~HEREDOC
+    +---+     
+    |   |     WORD:  #{secret_word}
+    |   0      
+    |  /|\    GUESS:
+    |  / \     
+    |         MISSES:
+    =========
+    HEREDOC
+  end
 end
