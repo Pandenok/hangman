7ff365f287734f3132197260fa8c0df02531ca6f
pandenok <dimitri.shatunin@gmail.com>
2020-07-04 18:18:20 +0200

    Load library and choose a random word between 5 and 12 chars long

--- lib/host.rb
+++ lib/host.rb
@@ -1,2 +1,20 @@
 class Host
+  attr_accessor :contents
+  attr_reader :secret_word
+  
+  def initialize
+    puts "Host initialized."
+  end
+  
+  def load_library
+    puts "Loading library..."
+    library = File.open "5desk.txt"
+    @contents = library.read
+  end
+  
+  def choose_word
+    puts "Choosing word..."
+    @secret_word = contents.scan(/\w+/).select { |word| word.length.between?(5, 12) }.sample
+    puts "Secret word is #{@secret_word}"
+  end
 end
