81c25e05a53d9013b123fed1b86f7af725323a5d
pandenok <dimitri.shatunin@gmail.com>
2020-07-04 18:19:26 +0200

    Show pattern with placeholder in base of length of secret word

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,15 @@
 class Board
+  include Gallow
+  PLACEHOLDER = ' _ '
   
+  attr_accessor :pattern
+  
+  def initialize
+    @pattern = pattern
+  end
+  
+  def show_pattern(secret_word)
+    puts "Secret word is ready to be guessed."
+    @pattern = secret_word.split('').map { |letter| letter = PLACEHOLDER }.join(' ')
+  end
 end
