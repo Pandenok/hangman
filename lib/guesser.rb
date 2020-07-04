f9772f9234356bae7025ae83a9cdb9f7e9744c95
pandenok <dimitri.shatunin@gmail.com>
2020-07-04 18:20:14 +0200

    Make guess and validate input with REGEX

--- lib/guesser.rb
+++ lib/guesser.rb
@@ -1,2 +1,20 @@
 class Guesser
+  attr_reader :guess
+
+  def initialize; end
+  
+  def make_guess
+    print "Make guess: "
+    @player_input = gets.chomp.upcase
+    valid_input?(@player_input)
+    @guess = @player_input
+  end
+
+  def valid_input?(player_input)
+    until player_input.match?(/[A-Z]/)
+      puts "Invalid input"
+      puts "Please type one letter of the latin alphabet"
+      player_input = gets.chomp.upcase
+    end
+  end
 end
