0811beeaa005f174ca805c83cd27267762a22330
pandenok <dimitri.shatunin@gmail.com>
2020-07-05 15:25:11 +0200

    Change wording: player -> guesser

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,6 @@
 require_relative 'game.rb'
 require_relative 'display.rb'
-require_relative 'player.rb'
+require_relative 'guesser.rb'
 require_relative 'board.rb'
 require_relative 'host.rb'
 require_relative 'gallow.rb'
