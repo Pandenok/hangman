ac9096fc917c0a0b20d6e08005617c3cf1103797
pandenok <dimitri.shatunin@gmail.com>
2020-07-05 15:24:17 +0200

    Make guess case insensitive

--- lib/guesser.rb
+++ lib/guesser.rb
@@ -5,16 +5,16 @@ class Guesser
   
   def make_guess
     print "Make guess: "
-    @player_input = gets.chomp.upcase
+    @player_input = gets.chomp.downcase
     valid_input?(@player_input)
     @guess = @player_input
   end
 
   def valid_input?(player_input)
-    until player_input.match?(/[A-Z]/)
+    until player_input.match?(/[a-z]/)
       puts "Invalid input"
       puts "Please type one letter of the latin alphabet"
-      player_input = gets.chomp.upcase
+      player_input = gets.chomp.downcase
     end
   end
 end
