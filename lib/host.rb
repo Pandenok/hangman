6e4802ee0f86407419a842d0e5fa1dec22cccd5d
pandenok <dimitri.shatunin@gmail.com>
2020-07-06 18:32:05 +0200

    UI: report library loading and word generating

--- lib/host.rb
+++ lib/host.rb
@@ -1,20 +1,21 @@
 class Host
+  include Display
+  include Colorable
+
   attr_accessor :contents
   attr_reader :secret_word
   
-  def initialize
-    puts "Host initialized."
-  end
+  def initialize; end
   
   def load_library
-    puts "Loading library..."
+    print display_library_loading
     library = File.open "5desk.txt"
     @contents = library.read
   end
   
   def choose_word
-    puts "Choosing word..."
-    @secret_word = contents.scan(/\w+/).select { |word| word.length.between?(5, 12) }.sample.downcase
+    puts display_word_generating
+    @secret_word = contents.scan(/\w+/).select { |word| word.length.between?(5, 12) }.sample.upcase
     puts "Secret word is #{@secret_word}"
   end
 end
