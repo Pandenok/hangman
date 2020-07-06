9edf872c70a75db2d24d6e53a163517f5903d964
pandenok <dimitri.shatunin@gmail.com>
2020-07-06 19:33:57 +0200

    UI: color gallow in cyan, hangman figure change color with errors

--- lib/gallow.rb
+++ lib/gallow.rb
@@ -4,103 +4,103 @@ module Gallow
   def draw_gallow(pattern, guess, misses)
     <<~HEREDOC
 
-    +---+     
-    |        WORD:   #{pattern.join(' ')}
-    |          
-    |        GUESS:  #{guess}
-    |
-    |        MISSES: #{red("#{misses.join(', ')}")}  
-    =========
+    #{cyan(bold("+---+"))}     
+    #{cyan(bold("|"))}          WORD:   #{pattern.join(' ')}
+    #{cyan(bold("|"))}            
+    #{cyan(bold("|"))}          GUESS:  #{guess}
+    #{cyan(bold("|"))}  
+    #{cyan(bold("|"))}          MISSES: #{red("#{misses.join(', ')}")}  
+    #{cyan(bold("========="))}
     HEREDOC
   end
   
   def draw_head(pattern, guess, misses)
     <<~HEREDOC
 
-    +---+     
-    |         WORD:   #{pattern.join(' ')}  
-    |   0       
-    |         GUESS:  #{guess}
-    |         
-    |         MISSES: #{red("#{misses.join(', ')}")}
-    =========
+    #{cyan(bold("+---+"))}     
+    #{cyan(bold("|"))}          WORD:   #{pattern.join(' ')}  
+    #{cyan(bold("|"))}   #{lt_yellow("0")}       
+    #{cyan(bold("|"))}          GUESS:  #{guess}
+    #{cyan(bold("|"))}         
+    #{cyan(bold("|"))}          MISSES: #{red("#{misses.join(', ')}")}
+    #{cyan(bold("========="))}
     HEREDOC
   end
   
   def draw_body(pattern, guess, misses)
     <<~HEREDOC
 
-    +---+     
-    |         WORD:   #{pattern.join(' ')}
-    |   0       
-    |   |     GUESS:  #{guess}
-    |         
-    |         MISSES: #{red("#{misses.join(', ')}")}
-    =========
+    #{cyan(bold("+---+"))}     
+    #{cyan(bold("|"))}          WORD:   #{pattern.join(' ')}
+    #{cyan(bold("|"))}   #{lt_yellow("0")}      
+    #{cyan(bold("|"))}   #{lt_yellow("|")}      GUESS:  #{guess}
+    #{cyan(bold("|"))}         
+    #{cyan(bold("|"))}          MISSES: #{red("#{misses.join(', ')}")}
+    #{cyan(bold("========="))}
     HEREDOC
   end
   
   def draw_right_arm(pattern, guess, misses)
     <<~HEREDOC
 
-    +---+     
-    |          WORD:   #{pattern.join(' ')}  
-    |   0        
-    |   |\\     GUESS:  #{guess}  
-    |         
-    |          MISSES: #{red("#{misses.join(', ')}")}
-    =========
+    #{cyan(bold("+---+"))}     
+    #{cyan(bold("|"))}          WORD:   #{pattern.join(' ')}  
+    #{cyan(bold("|"))}   #{orange("0")}      
+    #{cyan(bold("|"))}   #{orange("|\\")}     GUESS:  #{guess}  
+    #{cyan(bold("|"))}         
+    #{cyan(bold("|"))}          MISSES: #{red("#{misses.join(', ')}")}
+    #{cyan(bold("========="))}
     HEREDOC
   end
   
   def draw_left_arm(pattern, guess, misses)
     <<~HEREDOC
 
-    +---+     
-    |          WORD:   #{pattern.join(' ')}
-    |   0       
-    |  /|\\     GUESS:  #{guess}
-    |         
-    |          MISSES: #{red("#{misses.join(', ')}")}
-    =========
+    #{cyan(bold("+---+"))}     
+    #{cyan(bold("|"))}          WORD:   #{pattern.join(' ')}
+    #{cyan(bold("|"))}   #{orange("0")}      
+    #{cyan(bold("|"))}  #{orange("/|\\")}     GUESS:  #{guess}
+    #{cyan(bold("|"))}      
+    #{cyan(bold("|"))}          MISSES: #{red("#{misses.join(', ')}")}
+    #{cyan(bold("========="))}
     HEREDOC
   end
   
   def draw_right_leg(pattern, guess, misses)
     <<~HEREDOC
 
-    +---+     
-    |          WORD:   #{pattern.join(' ')}
-    |   0       
-    |  /|\\     GUESS:  #{guess}
-    |    \\     
-    |          MISSES: #{red("#{misses.join(', ')}")}
-    =========
+    #{cyan(bold("+---+"))}     
+    #{cyan(bold("|"))}          WORD:   #{pattern.join(' ')}
+    #{cyan(bold("|"))}   #{lt_red("0")}      
+    #{cyan(bold("|"))}  #{lt_red("/|\\")}     GUESS:  #{guess}
+    #{cyan(bold("|"))}    #{lt_red("\\")}     
+    #{cyan(bold("|"))}          MISSES: #{red("#{misses.join(', ')}")}
+    #{cyan(bold("========="))}
     HEREDOC
   end
   
   def draw_left_leg(pattern, guess, misses)
     <<~HEREDOC
-    +---+     
-    |          WORD:   #{pattern.join(' ')}
-    |   0       
-    |  /|\\     GUESS:  #{guess}
-    |  / \\     
-    |          MISSES: #{red("#{misses.join(', ')}")}
-    =========
+    #{cyan(bold("+---+"))}     
+    #{cyan(bold("|"))}          WORD:   #{pattern.join(' ')}
+    #{cyan(bold("|"))}   #{red("0")}      
+    #{cyan(bold("|"))}  #{red("/|\\")}     GUESS:  #{guess}
+    #{cyan(bold("|"))}  #{red("/ \\")}     
+    #{cyan(bold("|"))}          MISSES: #{red("#{misses.join(', ')}")}
+    #{cyan(bold("========="))}
     HEREDOC
   end
   
   def draw_noose(pattern, guess, misses)
     <<~HEREDOC
-    
-    +---+     
-    |   |      WORD:   #{pattern.join(' ')}
-    |   0      
-    |  /|\\     GUESS:  #{guess}
-    |  / \\     
-    |          MISSES: #{red("#{misses.join(', ')}")}
-    =========
+
+    #{cyan(bold("+---+"))}     
+    #{cyan(bold("|"))}   #{red("|")}      WORD:   #{pattern.join(' ')}
+    #{cyan(bold("|"))}   #{red("0")}      
+    #{cyan(bold("|"))}  #{red("/|\\")}     GUESS:  #{guess}
+    #{cyan(bold("|"))}  #{red("/ \\")}     
+    #{cyan(bold("|"))}          MISSES: #{red("#{misses.join(', ')}")}
+    #{cyan(bold("========="))}
     HEREDOC
   end
 end
