d552e98c3cc56b57c1a326df97c09e561a00368d
pandenok <dimitri.shatunin@gmail.com>
2020-07-07 14:29:03 +0200

    Minor style/documentation refactoring with rubocop

--- lib/host.rb
+++ lib/host.rb
@@ -4,15 +4,15 @@ class Host
 
   attr_accessor :contents
   attr_reader :secret_word
-  
+
   def initialize; end
-  
+
   def load_library
     print display_library_loading
-    library = File.open "5desk.txt"
+    library = File.open '5desk.txt'
     @contents = library.read
   end
-  
+
   def choose_word
     puts display_word_generating
     @secret_word = contents.scan(/\w+/).select { |word| word.length.between?(5, 12) }.sample.upcase
