8ad1f4b4d0093ab62627d57261805073133ed281
pandenok <dimitri.shatunin@gmail.com>
2020-07-07 15:10:03 +0200

    Layout/Style refactoring with rubocop

--- lib/board.rb
+++ lib/board.rb
@@ -5,17 +5,17 @@ class Board
   include Display
   include Colorable
 
-  PLACEHOLDER = " _ "
-  
+  PLACEHOLDER = ' _ '
+
   attr_accessor :pattern, :misses
-  
+
   def initialize
     @pattern = pattern
     @misses = []
   end
-  
+
   def hide(secret_word)
-    @pattern = secret_word.split('').map { |letter| letter = bg_gray("#{PLACEHOLDER}") }
+    @pattern = secret_word.split('').map { bg_gray("#{PLACEHOLDER}") }
   end
 
   def letter_guessed?(secret_word, guess)
@@ -27,31 +27,31 @@ class Board
   end
 
   def letter_already_tried?(input)
-    if pattern.include?(green(underline("#{input.upcase}"))) || 
-        misses.include?(input.upcase)
+    if pattern.include?(green(underline("#{input.upcase}"))) ||
+       misses.include?(input.upcase)
       print display_notice_letter_tried(input)
       input
     end
   end
-  
-  def unhide_right_guess(secret_word, guess, pattern)
+
+  def unhide_right_guess(secret_word, guess)
     secret_word.split('').each_with_index do |letter, idx|
       if letter.eql?(guess)
         letter = green(underline("#{letter}"))
-        pattern[idx] = letter 
+        pattern[idx] = letter
       end
     end
   end
 
-  def unhide_secret_word(secret_word, guess, pattern)
+  def unhide_secret_word(secret_word)
     @pattern = secret_word.split('').map { |letter| green(underline("#{letter}")) }
   end
 
   def analyse_guess(secret_word, guess)
     if letter_guessed?(secret_word, guess)
-      unhide_right_guess(secret_word, guess, pattern)
+      unhide_right_guess(secret_word, guess)
     elsif word_guessed?(secret_word, guess)
-      unhide_secret_word(secret_word, guess, pattern) 
+      unhide_secret_word(secret_word)
     else
       misses << guess
     end
@@ -66,8 +66,7 @@ class Board
       4 => draw_left_arm(pattern, guess, misses),
       5 => draw_right_leg(pattern, guess, misses),
       6 => draw_left_leg(pattern, guess, misses),
-      7 => draw_noose(pattern, guess, misses),
+      7 => draw_noose(pattern, guess, misses)
     } [number]
   end
-
 end
